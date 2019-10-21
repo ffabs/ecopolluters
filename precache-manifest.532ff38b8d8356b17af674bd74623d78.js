2959e6716d70e97d322a38f510106946dad8b431
ffabs <fabioandferrari@gmail.com>
2019-10-21 20:25:02 +0200

    Updates

---
+++ precache-manifest.532ff38b8d8356b17af674bd74623d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd622a2b3a3293d796e69c9416243ef1",
+    "url": "/ecopolluters/index.html"
+  },
+  {
+    "revision": "18dbf0434c936daf7967",
+    "url": "/ecopolluters/static/css/main.95ccc8ae.chunk.css"
+  },
+  {
+    "revision": "76b87946bfe87ca5521f",
+    "url": "/ecopolluters/static/js/2.5e521909.chunk.js"
+  },
+  {
+    "revision": "18dbf0434c936daf7967",
+    "url": "/ecopolluters/static/js/main.38232136.chunk.js"
+  },
+  {
+    "revision": "1eba47adf22673e2cd75",
+    "url": "/ecopolluters/static/js/runtime-main.81421743.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ecopolluters/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c8fca2a80c71b7b213dd2ff796f2b2d.js"
+  "/ecopolluters/precache-manifest.532ff38b8d8356b17af674bd74623d78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecopolluters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
