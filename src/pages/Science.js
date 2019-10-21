95cf51482cc3f192d5accdff44532e6585191fe5
ffabs <fabioandferrari@gmail.com>
2019-10-21 23:33:03 +0200

    Google analytics in each page

--- src/pages/Science.js
+++ src/pages/Science.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
+import ReactGA from 'react-ga';
+
 function initializeReactGA() {
     ReactGA.initialize('UA-150607597-1');
     ReactGA.pageview('/science');
