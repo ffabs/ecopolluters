d045d2bef06982bb3ff075c4bcdbf4b5aeedbc18
ffabs <fabioandferrari@gmail.com>
2019-10-24 22:11:34 +0200

    replacing pollutions with flights and average 1 person flat

--- src/components/CO2.js
+++ src/components/CO2.js
@@ -10,7 +10,7 @@ class CO2 extends Component {
         <div>
           <h3> Greeen house gasses: {this.props.CO2.toFixed(2)} kg CO2eq </h3>
           <p>Distance with an average petrol car (5 liters per 100km): {(this.props.CO2/0.12).toFixed(2)} km</p>
-          <p>Distance with an Hummer (14.7 liters per 100km): {(this.props.CO2/0.351624).toFixed(2)} km</p>
+          <p>Flight minutes of an average plane: {(this.props.CO2/90*60).toFixed(2)} minutes</p>
         </div>
 
     );
--- src/components/Land.js
+++ src/components/Land.js
@@ -10,7 +10,7 @@ class Land extends Component {
         <div>
           <h3> Land usage: {this.props.land.toFixed(2)} square meters</h3>
           <p>Parking lots: {(this.props.land/25).toFixed(2)} </p>
-          <p>Average 1 person flat in HK: {(this.props.land/15).toFixed(2)}</p>
+          <p>Average 1 person flat in the world: {(this.props.land/52.6).toFixed(2)}</p>
         </div>
 
     );
