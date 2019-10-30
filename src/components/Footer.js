604c5e745ba319226c9c7b0e6817d80016332c60
ffabs <fabioandferrari@gmail.com>
2019-10-30 19:15:49 +0100

    facebook and linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Nav from '../components/Nav';
+import { SocialIcon } from 'react-social-icons';
 
 class Footer extends Component {
 
@@ -10,6 +11,8 @@ class Footer extends Component {
 
         <div className="header">
           <hr></hr>
+          <SocialIcon url="https://www.facebook.com/Ecopolluters-112149543528301/?eid=ARBpbIuZygo64Yd0W9hgh_6osBi68vc9NuK8dUm3DgKvpJfs4YslZcF-gwN_hRdpFJtxOLnTg5AcGCNQ" />
+          <SocialIcon url="https://www.linkedin.com/company/28890458" />
           <Nav />
           <p>Ecopolluters N.G.O. - All Rights Reserved</p>
         </div>
