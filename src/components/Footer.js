669ed884809ae6f9d0ceb8ef3ca9e8b9a9f58300
ffabs <fabioandferrari@gmail.com>
2019-11-01 11:21:52 +0100

    ecopolluters instead of Ecopolluters

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ class Footer extends Component {
           <SocialIcon className="icon" url="https://www.facebook.com/Ecopolluters-112149543528301/?eid=ARBpbIuZygo64Yd0W9hgh_6osBi68vc9NuK8dUm3DgKvpJfs4YslZcF-gwN_hRdpFJtxOLnTg5AcGCNQ" style={{ height: 75, width: 75 }}/>
           <SocialIcon className="icon" url="https://www.linkedin.com/company/28890458" style={{ height: 75, width: 75 }}/>
           <Nav />
-          <p>Ecopolluters N.G.O. - All Rights Reserved</p>
+          <p>ecopolluters N.G.O. - All Rights Reserved</p>
         </div>
 
     );
