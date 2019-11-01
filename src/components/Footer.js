df23b6707bef0a516c51821c42cd549133699e08
ffabs <fabioandferrari@gmail.com>
2019-11-01 13:48:13 +0100

    share on facebook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,9 @@ class Footer extends Component {
           <SocialIcon className="icon" url="https://www.facebook.com/Ecopolluters-112149543528301/?eid=ARBpbIuZygo64Yd0W9hgh_6osBi68vc9NuK8dUm3DgKvpJfs4YslZcF-gwN_hRdpFJtxOLnTg5AcGCNQ" style={{ height: 60, width: 60 }}/>
           <SocialIcon className="icon" url="https://twitter.com/ecopolluters" style={{ height: 60, width: 60 }}/>
           <SocialIcon className="icon" url="https://www.linkedin.com/company/28890458" style={{ height: 60, width: 60 }}/>
+          <div id="fb-root"></div>
+          <script async defer crossorigin="anonymous" src="https://connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v5.0"></script>
+          <div class="fb-share-button" data-href="https://ecopolluters.org/#/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fecopolluters.org%2F%23%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share on Facebook</a></div>
           <Nav />
           <p>ecopolluters N.G.O. - All Rights Reserved</p>
         </div>
