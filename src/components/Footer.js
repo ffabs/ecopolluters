a2b6572dea102af90d3a30799084ef85cef59839
ffabs <fabioandferrari@gmail.com>
2019-11-01 13:32:02 +0100

    moving linkedin at the end

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,8 @@ class Footer extends Component {
 
         <div className="header footer">
           <SocialIcon className="icon" url="https://www.facebook.com/Ecopolluters-112149543528301/?eid=ARBpbIuZygo64Yd0W9hgh_6osBi68vc9NuK8dUm3DgKvpJfs4YslZcF-gwN_hRdpFJtxOLnTg5AcGCNQ" style={{ height: 60, width: 60 }}/>
-          <SocialIcon className="icon" url="https://www.linkedin.com/company/28890458" style={{ height: 60, width: 60 }}/>
           <SocialIcon className="icon" url="https://twitter.com/ecopolluters" style={{ height: 60, width: 60 }}/>
+          <SocialIcon className="icon" url="https://www.linkedin.com/company/28890458" style={{ height: 60, width: 60 }}/>
           <Nav />
           <p>ecopolluters N.G.O. - All Rights Reserved</p>
         </div>
