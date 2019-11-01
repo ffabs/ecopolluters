6baa9b5f56d38acbef2738df11da27a4a250eaec
ffabs <fabioandferrari@gmail.com>
2019-11-01 12:08:29 +0100

    twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,6 +12,7 @@ class Footer extends Component {
         <div className="header footer">
           <SocialIcon className="icon" url="https://www.facebook.com/Ecopolluters-112149543528301/?eid=ARBpbIuZygo64Yd0W9hgh_6osBi68vc9NuK8dUm3DgKvpJfs4YslZcF-gwN_hRdpFJtxOLnTg5AcGCNQ" style={{ height: 75, width: 75 }}/>
           <SocialIcon className="icon" url="https://www.linkedin.com/company/28890458" style={{ height: 75, width: 75 }}/>
+          <SocialIcon className="icon" url="https://twitter.com/ecopolluters" style={{ height: 75, width: 75 }}/>
           <Nav />
           <p>ecopolluters N.G.O. - All Rights Reserved</p>
         </div>
