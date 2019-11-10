31ffe8a649b1cbe7831e81dbd51f44d33c1e4371
ffabs <fabioandferrari@gmail.com>
2019-11-10 19:25:17 +0100

    footer margin improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
               SHARE ON FACEBOOK ➡️
             </a>
           </div>
-          <div>
+          <div className="socials">
             <SocialIcon className="icon" url="https://www.facebook.com/Ecopolluters-112149543528301/?eid=ARBpbIuZygo64Yd0W9hgh_6osBi68vc9NuK8dUm3DgKvpJfs4YslZcF-gwN_hRdpFJtxOLnTg5AcGCNQ" style={{ height: 50, width: 50 }} bgColor="#98A1AB"/>
             <SocialIcon className="icon" url="https://twitter.com/ecopolluters" style={{ height: 50, width: 50 }} bgColor="#98A1AB"/>
             <SocialIcon className="icon" url="https://www.linkedin.com/company/28890458" style={{ height: 50, width: 50 }} bgColor="#98A1AB"/>
