0dc913386d9ffb9643857869f23314c77c968f22
ffabs <fabioandferrari@gmail.com>
2020-01-08 14:56:04 +0100

    impact-data json file & removing loader data

--- src/components/Pollution.js
+++ src/components/Pollution.js
@@ -4,45 +4,21 @@ import Grams from '../components/Grams';
 import CO2 from '../components/CO2';
 import Land from '../components/Land';
 import Water from '../components/Water';
+import Data from '../impact-data.json';
 
 
 class Pollution extends Component {
-  
-    // constructor() {
-	// 	super();
-	// 	this.state = {
-    //         loading: true
-	// 	};
-    // }
 
     render() {
-        // setTimeout(function(){
-        //     // this.setState({loading: false});
-        //     // this.setProps({loading: false});
-        //   }.bind(this), 300);
 
         return (
 
             <div className="pollutions">
-{/* 
-                <button 
-                    type="button" 
-                    onClick={this.props.handleLoading}>
-                    LOADING
-                </button>
 
-                {this.props.loading === true &&
-                    <div class="loader"></div>
-                } */}
-
-                {/* {this.props.loading === false && */}
-                    {/* <div> */}
-                        <Grams grams={this.props.grams}/>
-                        <CO2 CO2={this.props.grams*0.09948}/>
-                        <Water water={this.props.grams*1.4512}/>
-                        <Land land={this.props.grams*0.32621}/>
-                    {/* </div> */}
-                {/* } */}
+                <Grams grams={this.props.grams}/>
+                <CO2 CO2={this.props.grams*Data.beef.co2}/>
+                <Water water={this.props.grams*Data.beef.water}/>
+                <Land land={this.props.grams*Data.beef.land}/>
 
             </div>
 
